ebeefc7d8995a86eaf30331b1196849928195682
Alexis Ferrandis <alexis.1.ferrandis@gmail.com>
2022-03-22 17:54:21 +0100

    Character animations

--- Sprite.js
+++ Sprite.js
@@ -20,21 +20,82 @@ class Sprite {
 
 		// configure animation and initial state
 		this.animation = config.animation || {
-			idleDown: [[0, 0]],
+			"idle-down": [[0, 0]],
+			"idle-left": [[0, 1 + 0.5]],
+			"idle-right": [[0, 2 + 1]],
+			"idle-up": [[0, 3 + 1.5]],
+			"walk-down": [
+				[1, 0],
+				[0, 0],
+				[3, 0],
+				[0, 0],
+			],
+			"walk-left": [
+				[1, 1.5],
+				[0, 1.5],
+				[3, 1.5],
+				[0, 1.5],
+			],
+			"walk-right": [
+				[1, 3],
+				[0, 3],
+				[3, 3],
+				[0, 3],
+			],
+			"walk-up": [
+				[1, 4.5],
+				[0, 4.5],
+				[3, 4.5],
+				[0, 4.5],
+			],
 		};
-		this.currentAnimation = config.currentAnimation || "idleDown";
+		this.currentAnimation = "walk-down"; // config.currentAnimation || "idle-down";
 		this.currentAnimationFrame = 0;
+
+		this.animationFrameLimit = config.animationFrameLimit || 8;
+		this.animationFrameProgress = this.animationFrameLimit;
 
 		// reference the game object
 		this.gameObject = config.gameObject;
 	}
 
+	get frame() {
+		return this.animation[this.currentAnimation][this.currentAnimationFrame];
+	}
+
+	setAnimation(key) {
+		if (this.currentAnimation !== key) {
+			this.currentAnimation = key;
+			this.currentAnimationFrame = 0;
+			this.animationFrameProgress = this.animationFrameLimit;
+		}
+	}
+
+	updateAnimationProgress() {
+		// Downtick frame progress
+		if (this.animationFrameProgress > 0) {
+			this.animationFrameProgress -= 1;
+			return;
+		}
+
+		// reset counter
+		this.animationFrameProgress = this.animationFrameLimit;
+		this.currentAnimationFrame += 1;
+
+		if (this.frame === undefined) {
+			this.currentAnimationFrame = 0;
+		}
+	}
+
 	draw(ctx) {
-		const x = this.gameObject.x;
-		const y = this.gameObject.y;
+		const x = this.gameObject.x - 2;
+		const y = this.gameObject.y - 2;
 
 		this.isShadowLoaded && ctx.drawImage(this.shadow, x + 2, y + 18, 12, 6);
 
-		this.isLoaded && ctx.drawImage(this.image, 0, 0, 32, 48, x, y, 16, 24);
+		const [frameX, frameY] = this.frame;
+
+		this.isLoaded && ctx.drawImage(this.image, frameX * 32, frameY * 32, 32, 48, x, y, 16, 24);
+		this.updateAnimationProgress();
 	}
 }
